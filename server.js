288a4e7d7858b586318544bdbd7ec18e73f1d6de
Farid Hajnal <faridhajnal14@hotmail.com>
2016-05-09 23:28:59 +0200

    Refactor code using underscore

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-var express = require('express'); //express npm module
+var express = require('express'); // npm modules
 var bodyparser = require('body-parser');
+var _ = require('underscore'); //similar to linq on C#
+
+
+
 var app = express(); //start express
 var PORT = process.env.PORT || 3000; //environment variable from heroku
 
@@ -49,12 +53,12 @@ app.get('/todos', function(request,response){
 
 app.get('/todos/:id', function(request,response){
     
-    var todoId = request.params.id;
+    var todoId = parseInt(request.params.id, 10);
     
     console.log('Asking for todo with id: ' + todoId); //params is short for url paramteres
 
-    var match = false;
-    todos.forEach(function(element) {
+    var matchedTodo = _.findWhere(todos, {id:todoId});//find where => 1 result (linq)
+    /*todos.forEach(function(element) {
         
         if(element.id==todoId){ // === checks Type!
             
@@ -63,19 +67,33 @@ app.get('/todos/:id', function(request,response){
             response.json(element);
             
         } 
-    });
+    }); 'NORMAL IMPLEMENTATION' 
     
    if(!match) {
        console.log('404 Not found!');
        response.status(404).send();
    }
+   
+   */
+   if(matchedTodo) response.json(matchedTodo);
+   else response.status(404).send();
     
     
 });
 
 app.post('/todos', function(request,response){ //body parser npm needed
     
-    var body = request.body;
+    var body = _.pick(request.body, 'description', 'completed'); //pick from body only description and completed keys
+    
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+        
+        return response.status(400).send('Format Problem, check'); //request cannot be completed
+        
+    }
+    
+    body.description = body.description.trim(); //discard additional spaces(beginning , end)
+    
+    
     console.log('description: ' + body.description);
     /*todos.push({
         
